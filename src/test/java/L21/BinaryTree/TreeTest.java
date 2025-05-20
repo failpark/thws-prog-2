e71148683c61d86e614f57a1829c67fe3cdb2e8c
phedias <daniel.borgs@noxa.de>
2025-05-20 13:19:01 +0200

    feat(test): add test from excercise

--- src/test/java/L21/BinaryTree/TreeTest.java
+++ src/test/java/L21/BinaryTree/TreeTest.java
@@ -24,4 +24,14 @@ public class TreeTest {
 		String expected = "Daniel\nAnna\nNaomi\nRaffi\n";
 		assertEquals(expected, tree.toString());
 	}
+
+	@Test
+	public void validTree_String_ex() {
+		Tree<String> tree = new Tree<>("Cool");
+		tree.root.left = tree.get_node("Aarson");
+		tree.root.right = tree.get_node("Zylla");
+		tree.root.right.left = tree.get_node("Garret");
+		String expected = "Aarson\nCool\nGarret\nZylla\n";
+		assertEquals(expected, tree.toString());
+	}
 }
